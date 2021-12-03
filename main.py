a3f42cad2046656013896df2b50c025a45971778
Klay Clarke <klayaclarke@gmail.com>
2021-12-02 20:03:02 -0500

    change url - use web archive because the original site might be generating html code using JS (difficult to parse, if possible at all)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.empireonline.com/movies/features/best-movies-2')
+response = requests.get('https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/')
 empire_web_content = response.text
 
 soup = BeautifulSoup(empire_web_content, 'html.parser')
-print(soup)
