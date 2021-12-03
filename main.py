381d0aade6364f1f1f98b72f8c328bbab38c3add
Klay Clarke <klayaclarke@gmail.com>
2021-12-02 20:37:41 -0500

    create list of movie titles in order of webpage (ranked from 100 to 1) - reverse list using .reverse() method - create txt file and write to it for each movie in list.

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ response = requests.get('https://web.archive.org/web/20200518073855/https://www.
 empire_web_content = response.text
 
 soup = BeautifulSoup(empire_web_content, 'html.parser')
+
+movies_tag = soup.find_all('h3', class_='title')
+movies = [movie.text for movie in movies_tag]
+movies.reverse()
+
+with open('movies-ranked.txt', 'w') as file:
+    for movie in movies:
+        file.writelines(f'{movie}\n')
+
